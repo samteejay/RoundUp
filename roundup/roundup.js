2697c05aa320aa07c5b2c86eefcd3d30594eea9c
Samuel Tijani <omalitijani@gmail.com>
2020-02-10 14:57:42 +0100

    Init commit

---
+++ roundup/roundup.js
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Roundup</title>
+</head>
+
+<body>
+<script>
+
+scoreAvg = Math.round(.019999999);
+alert(scoreAvg);
+
+scoreAvg = Math.ceil(.019999999);
+alert(scoreAvg);
+
+scoreAvg = Math.floor(.019999999);
+alert(scoreAvg);
+
+	
+</script>
+
+</body>
+</html>
